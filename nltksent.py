bc3ce3723c7e1de4c47c19cb9ae43e8c2b78ab2b
Siddharth Gautam <47804254+Siddharth-Gautam0@users.noreply.github.com>
2021-05-29 12:53:33 +0530

    Add files via upload

---
+++ nltksent.py
@@ -0,0 +1,8 @@
+import nltk
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+
+train_text = state_union.raw("2005-GWBush.txt")
+sample_text = state_union.raw("2006-GWBush.txt")
+
+#print(train_text)
